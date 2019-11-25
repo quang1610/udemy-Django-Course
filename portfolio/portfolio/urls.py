d1782ac2bd6c3a878c4fede3a3710ba758a4bd2e
quang1610 <nguyenducquang1610@gmail.com>
2019-11-24 22:05:19 -0600

    finish upto lecture 36

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,4 +1,4 @@
-"""portfolio-project URL Configuration
+"""portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', admin.site.urls)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
